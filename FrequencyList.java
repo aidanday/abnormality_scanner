a275752d384d0036026d30e41bbf31993c85203d
aidanday <aidanday4@gmail.com>
2017-02-04 20:52:31 -0500

    Write FrequencyList.insert()

--- FrequencyList.java
+++ FrequencyList.java
@@ -6,14 +6,26 @@ public FrequencyList {
     Node * front_i;
 
     private Node {
-        Color c_i;
-	int freq_i;
-        Node * next_i;
-	Node * prev_i;
+        public Color c_i;
+	public int freq_i;
+        public Node * next_i;
+	public Node * prev_i;
+
+	Node(Color c_p, next_p, prev_i) {
+	    this.c_i = c_p;
+	    this.freq_i = 1;
+	    this.next_i = next_p;
+	    this.prev_i = prev_p;
+	}
+
+	public void insert(Color c_p, int threshold_p) {
+	    // TODO
+	}
     }
 
     public void insert(Color c_p, int threshold_p) {
-        // TODO
+        if (null == front_i) front_i = new Node(c_p, null, null);
+	else (*front).insert(c_p, threshold_p);
     }
 
     public Color [] head(int n_p) {
