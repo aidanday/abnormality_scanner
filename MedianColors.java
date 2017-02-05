bb90744006c23c4199dc5ab53c22d6405ae8f43a
MarcinZR <marcin.r111@gmail.com>
2017-02-04 21:46:41 -0500

    Fixed 'fl' declaration

--- MedianColors.java
+++ MedianColors.java
@@ -6,17 +6,17 @@ import java.awt.Color;
 class MedianColors {
     public static Color [] find(BufferedImage image_p, int num_expected_p) {
         int height = image_p.getHeight();
-	    int width = image_p.getWidth();
-	    final int threshold = 40;
+        int width = image_p.getWidth();
+        final int threshold = 40;
 
-        FrequencyList fl;
+        FrequencyList fl = new FrequencyList();
 
         for(int i = 0 ; i < height ; i++) {
             for(int j = 0 ; j < width ; j++) {
-                fl.insert(image_p.getRGB(j,i), threshold);
+                fl.insert(new Color(image_p.getRGB(j,i)), threshold);
             } // end for(j)
         } // end for(i)
 
-	    return fl.head(num_expected_p);
+        return fl.head(num_expected_p);
     }
 }
