56fe2846f738248de3a4e0ed60f510d2cc972df0
Aidan Day <aidanday4@gmail.com>
2017-02-04 20:41:36 -0500

    Add threshold default to MedianColors.java

--- MedianColors.java
+++ MedianColors.java
@@ -7,12 +7,13 @@ public MedianColors {
     public static Color [] find(BufferedImage image_p, int num_expected_p) {
         int height = image_p.getHeight();
 	int width = image_p.getWidth();
+	final int threshold = 40;
 
 	FrequencyList fl;
 
 	for(int i = 0 ; i < height ; i++) {
 	    for(int j = 0 ; j < width ; j++) {
-	        fl.insert(image_p.getRGB(j,i));
+	        fl.insert(image_p.getRGB(j,i), threshold);
 	    } // end for(j)
 	} // end for(i)
 
