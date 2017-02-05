304f3050a047a55eb45b37fbc0e1303abd1cae32
aidanday <aidanday4@gmail.com>
2017-02-04 19:11:17 -0500

    Implement basic color_diff, not accounting for blended colors

--- MapHeat.java
+++ MapHeat.java
@@ -35,9 +35,31 @@ public class MapHeat {
 	return heatimg;
     }
 
-    private int color_diff(Color color1_p, Color colors_p) {
-        return 0;
-	//TODO
+    private int color_diff(Color color1_p, Color color2_p) {
+	int current = color1_p.getRed() - color2_p.getRed();
+	if (current < 0) current = -current;
+	int total = current;
+	current = color1_p.getGreen() - color2_p.getGreen();
+	if (current < 0) current = -current;
+	total += current;
+	current = color1_p.getBlue() - color2_p.getBlue();
+	if (current < 0) current = -current;
+	total += current;
+	total /= 3;
+	return total;
+    }
+
+    private int color_diff(Color color_p, Color [] colors_p) {
+        int length = colors_p.length;
+        int min_diff = color_diff(color_p, colors_p[0]);
+	int temp;
+
+	for (int i = 1 ; i < length ; i++) {
+	    temp = color_diff(color_p, colors_p[i]);
+	    if (temp < min_diff) min_diff = temp;
+	} // end for(i)
+
+	return min_diff;
     }
 
 }
